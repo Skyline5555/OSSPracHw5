d9e66e4b95d25c7feaff8a0ed5040b0d767b2f08
Jinho <john3176@dongguk.edu>
2023-09-25 16:20:52 +0900

    2023-09-25 실습과제 #5 5

--- Main.java
+++ Main.java
@@ -5,14 +5,18 @@ public class Main{
         Scanner scan = new Scanner(System.in);
         String studentName;
         String studentNumber;
+        String studentMajor;
 
         System.out.print("이름을 입력하세요: ");
         studentName = scan.next();
         System.out.print("학번을 입력하세요: ");
         studentNumber = scan.next();
+        System.out.print("학과를 입력하세요: ");
+        studentMajor = scan.next();
 
         System.out.println("<출력>");
         System.out.println("이름: " + studentName);
         System.out.println("학번: " + studentNumber);
+        System.out.println("학과: " + studentMajor);
     }
 }
